ecc41d41290ca28eec62c9d888d90d72fade3d4d
mercy34mercy <is0558fs@ed.ritsumei.ac.jp>
2023-01-05 18:11:46 +0900

    とりあえず移行が完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import me from './assets/me.jpg';
+import twitter from './assets/logo_twitter.png'
+import facebook from './assets/logo_facebook.png'
+import github from './assets/logo_github.png'
+import linkedin from './assets/logo_linkedin.png'
+import instagram from './assets/logo_instagram.png'
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="home">
+
+        <div className="maincontainer">
+          <div className="photoarea img-wrap">
+            <img className="myphoto" src={me} alt="..." />
+          </div>
+          <div className="namearea">
+            <p className="vertical">Masashi &nbsp;&nbsp;Kobayashi</p>
+          </div>
+          </div>
+
+          <div className="img-container">
+            <a href="https://www.facebook.com/profile.php?id=100044776764162"><img src={facebook} className="logo" alt="..." /></a>
+            <a href="https://twitter.com/mashikotyan34"><img src={twitter} className="logo" alt="..." /></a>
+            <a href="https://www.instagram.com/mercy34r/"><img
+              src={instagram}
+              className="logo"
+              alt="..."
+            /></a>
+            <a href="https://github.com/mercy34mercy"><img src={github} className="logo" alt="..." /></a>
+            <a href="https://www.linkedin.com/in/%E9%9B%85%E5%8F%B2-%E5%B0%8F%E6%9E%97-92aaa91a7/"><img src={linkedin} className="logo" alt="..." /></a>
+          </div>
+     
     </div>
-  );
+  )
 }
+
 
 export default App;
